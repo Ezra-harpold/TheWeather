023bcfe2bde11bf59ad5462072a404d0bfc83797
Ezra Harpold <ezra.harpold@gmail.com>
2020-06-26 09:43:54 -0500

    Adding Dagger Hilt dependency injection

--- app/src/main/java/com/example/theweather/ui/MainActivity.kt
+++ app/src/main/java/com/example/theweather/ui/MainActivity.kt
@@ -1,13 +1,32 @@
-package com.example.theweather.ui
+package ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProviders
 import com.example.theweather.R
+
 
 class MainActivity : AppCompatActivity() {
 
+
+    lateinit var viewModel: MainActivityViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val viewModel: MainActivityViewModel = ViewModelProviders.of(this).get<MainActivityViewModel>(
+            MainActivityViewModel::class.java
+        )
+
+
+        //Coroutines.main {
+         //   val forecast = viewModel.forecast.await()
+          //  forecast.observe(this, Observer {
+           // Toast.makeText(this,it.size.toString(),Toast.LENGTH_LONG).show()
+           // })
+        //}
     }
+
 }
+
+
---
+++ app/src/main/java/com/example/theweather/ui/MainActivityViewModel.kt
@@ -0,0 +1,12 @@
+package ui
+
+import androidx.lifecycle.ViewModel
+import com.example.theweather.data.repositories.HourlyRepository
+import com.example.theweather.util.lazyDeferred
+
+class MainActivityViewModel (
+): ViewModel(){
+   // val forecast by lazyDeferred {
+    //    repository.getHourlyForecast(18)
+    //}
+}
