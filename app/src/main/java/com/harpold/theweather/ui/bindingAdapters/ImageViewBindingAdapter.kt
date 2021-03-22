4c974efc31ccc4fa7b68fabff08c897cae418664
Ezra Harpold <ezraharpold@Ezras-MBP.home>
2021-03-22 14:35:07 -0500

    Updated ImageViewBindingAdapter

--- app/src/main/java/com/harpold/theweather/ui/bindingAdapters/ImageViewBindingAdapter.kt
+++ app/src/main/java/com/harpold/theweather/ui/bindingAdapters/ImageViewBindingAdapter.kt
@@ -9,10 +9,23 @@ object ImageViewBindingAdapter {
     @BindingAdapter("imageCode")
     @JvmStatic
     fun bindImageCode(view: ImageView, code: String){
-        //TODO
-        // Add when loop to set the imageViews Drawable resource based on the icon code we
+        // The loop to sets the imageViews Drawable resource based on the icon code we
         // get from the forecast entities, icon is is stored in the entity's embedded Weather object
-        when {
-            code =="11d" -> view.setImageResource(R.drawable.icon_thunderstorm)
+        when (code) {
+            "13d" -> view.setImageResource(R.drawable.icon_snow)
+            "11d" -> view.setImageResource(R.drawable.icon_thunderstorm)
+            "10d" -> view.setImageResource(R.drawable.icon_rainy)
+            "09d" -> view.setImageResource(R.drawable.icon_rainy)
+            "04d" -> view.setImageResource(R.drawable.icon_cloudy)
+            "04n" -> view.setImageResource(R.drawable.icon_cloudy)
+            "03d" -> view.setImageResource(R.drawable.icon_cloudy)
+            "03n" -> view.setImageResource(R.drawable.icon_cloudy)
+            "02d" -> view.setImageResource(R.drawable.ic_partly_sunny)
+            "02n" -> view.setImageResource(R.drawable.icon_cloudy_night)
+            "01d" -> view.setImageResource(R.drawable.icon_sunny)
+            "01n" -> view.setImageResource(R.drawable.icon_moon)
+            else -> view.setImageResource(R.drawable.icon_cloudy)
+
+            
         }
     }}
